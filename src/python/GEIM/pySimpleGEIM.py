acb658de8df78817562821d491f916db15d919f0
Mahmudul Hasan Tamim <mahmudulh.tamim@gmail.com>
2025-06-30 17:18:47 -0500

    online complete

--- src/python/GEIM/pySimpleGEIM.py
+++ src/python/GEIM/pySimpleGEIM.py
@@ -29,13 +29,13 @@ class pyGEIM_offline:
     def __init__(self, snaps_obj, rank, norm_type="L2"):
         self.snaps_obj = snaps_obj
         self.tot_cells_per_snap = self.snaps_obj.tot_cells_per_snap
-        snaps_array = np.asarray(self.snaps_obj.snapshot_matrix_2D).copy()
+        snaps_array = np.asarray(self.snaps_obj.snapshot_matrix_2D_training).copy()
     
         self.__snaps = snaps_array
         self.rank = rank
         self.list_fields = self.snaps_obj.list_fields_paths
         self.list_field_to_range_cells = self.snaps_obj.list_field_to_range_cells
-        self.Nsnaps = snaps_obj.Nsnapshots
+        self.Nsnaps = snaps_obj.Nsnapshots_training
         self.Nfields = len(self.list_fields)
         self.dict_centroids_and_volumes_by_region = self.snaps_obj.dict_centroids_and_volumes_by_region
         self.norm_type = norm_type
@@ -49,7 +49,7 @@ class pyGEIM_offline:
         self.indices_sensor_snaps = np.zeros(self.rank, dtype=int)
         self.matrix_holding_bases = np.zeros((self.tot_cells_per_snap, self.rank))
         self.indices_position_sensors = np.zeros((self.rank), dtype=int)
-        self.__array_indices_maximizing_position = np.zeros((self.rank), dtype=int)
+        self.array_indices_maximizing_position = np.zeros((self.rank), dtype=int)
         self.scaling_factor = np.zeros((self.rank), dtype=float)
             
     def run_algorithm(self):
@@ -83,17 +83,17 @@ class pyGEIM_offline:
         self.matrix_holding_bases[:, count_basis] = copy.deepcopy(self.__residual_snaps[:, basis_snap_index])
         (index_start, index_end) = self.list_field_to_range_cells[sensor_field_index]
         index_maximizing_position = np.argmax(np.abs(self.matrix_holding_bases[index_start : index_end + 1, count_basis]))
-        self.__array_indices_maximizing_position[count_basis] = int(index_maximizing_position + index_start)
+        self.array_indices_maximizing_position[count_basis] = int(index_maximizing_position + index_start)
         self.indices_position_sensors[count_basis] = index_maximizing_position
         self.indices_sensor_fields[count_basis] = sensor_field_index
         self.indices_sensor_snaps[count_basis] = basis_snap_index
-        self.scaling_factor[count_basis] = self.matrix_holding_bases[self.__array_indices_maximizing_position[count_basis], count_basis]
+        self.scaling_factor[count_basis] = self.matrix_holding_bases[self.array_indices_maximizing_position[count_basis], count_basis]
 
     def __build_A(self, count_basis):
         for i in range(count_basis + 1):
                 for j in range(count_basis + 1):
                     if i == count_basis or j == count_basis:
-                        self.A[i, j] = self.matrix_holding_bases[self.__array_indices_maximizing_position[i], j] / self.scaling_factor[i]
+                        self.A[i, j] = self.matrix_holding_bases[self.array_indices_maximizing_position[i], j] / self.scaling_factor[i]
 
     def __coord_maximizing_snap_finder(self, norms):
         coord_max = np.unravel_index(np.argmax(norms), norms.shape)
@@ -102,14 +102,30 @@ class pyGEIM_offline:
     def __reconstruct_training_space(self, count_basis):
         mat_A = self.A[:count_basis + 1, :count_basis + 1]
         for index_snap in range(self.Nsnaps):
-            b = np.asarray(self.__snaps[self.__array_indices_maximizing_position[:count_basis + 1], index_snap]) / self.scaling_factor[:count_basis + 1]
+            b = np.asarray(self.__snaps[self.array_indices_maximizing_position[:count_basis + 1], index_snap]) / self.scaling_factor[:count_basis + 1]
             d_i = np.linalg.solve(mat_A, b)
             self.__J[:, index_snap] = self.matrix_holding_bases[:, :count_basis + 1] @ d_i
     
 class pyGEIM_online:
-    def __init__(self, offline_object, snaps_object):
+    def __init__(self, offline_object, snaps_object, rank_upto):
         self.offline_object = offline_object
+        self.A = self.offline_object.A
+        self.array_indices_maximizing_position = self.offline_object.array_indices_maximizing_position
+        self.rank_upto = rank_upto
+        self.scaling_factor = self.offline_object.scaling_factor
+        self.matrix_holding_bases = self.offline_object.matrix_holding_bases
         self.snaps_object = snaps_object
+        self.snapshot_testing = self.snaps_object.snapshot_matrix_2D_testing
+        self.Nsnaps = self.snapshot_testing.shape[1]
+        self.J = np.zeros(self.snapshot_testing.shape)
+        self.reconstruct_test_space()
+        
+    def reconstruct_test_space(self):
+        for index_snap in range(self.Nsnaps):
+            b = np.asarray(self.snapshot_testing[self.array_indices_maximizing_position[:self.rank_upto], index_snap]) / self.scaling_factor[:self.rank_upto]
+            d_i = np.linalg.solve(self.A, b)
+            self.J[:, index_snap] = self.matrix_holding_bases[:, :self.rank_upto] @ d_i
+    
 
 
 # Initialise the snapshot manager:
