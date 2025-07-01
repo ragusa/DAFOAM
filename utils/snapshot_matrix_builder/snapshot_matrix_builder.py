00e3ffee7c7d16605131f2c708c112c91bf3833e
Mahmudul Hasan Tamim <mahmudulh.tamim@gmail.com>
2025-07-01 17:00:25 -0500

    commented

--- utils/snapshot_matrix_builder/snapshot_matrix_builder.py
+++ utils/snapshot_matrix_builder/snapshot_matrix_builder.py
@@ -3,6 +3,7 @@ import fluidfoam
 import os
 import copy
 
+#to get 'regionName' and 'fieldName' from a constructed field name 'regionName/fieldName'
 def split_on_slash(input_string):
         if '/' in input_string:
             before, after = input_string.split('/', 1)
@@ -11,6 +12,9 @@ def split_on_slash(input_string):
             after = input_string
         return before, after
 
+#This class takes an instance of the snapshot_manager class object (a module/class defined in utils/snapshot_manager) along with
+# all the field paths of concern (be that observable or non-observable). It should be noted that the matrices are build at the initialization. Moreover, it creates two sets of matrices 
+# one for training and another for testing 
 class Snapshot_matrix_builder:
     def __init__(self, snapshot_manager_object, list_fields_paths):
         self.sm_obj = snapshot_manager_object
@@ -27,9 +31,13 @@ class Snapshot_matrix_builder:
         self._store_mesh_data()
         self._determine_tot_cells_per_snap()
 
+        #build training matrices
         self.snapshot_matrix_2D_training, self.snapshot_matrix_3D_training = self._build_snapshot_matrices(list_snap_paths=self.list_chosen_snapshot_paths)
+        #build testing matrices
         self.snapshot_matrix_2D_testing, self.snapshot_matrix_3D_testing = self._build_snapshot_matrices(list_snap_paths=self.list_unchosen_snapshot_paths)
     
+
+    #This method extracts the necessary mesh data and save it in a dictionary where the keys represent regions.
     def _store_mesh_data(self):
         self.dict_mesh_by_region = {}
         self.dict_centroids_and_volumes_by_region = {}
@@ -48,6 +56,9 @@ class Snapshot_matrix_builder:
             # centroids = self.dict_centroids_and_volumes_by_region[region][0]
             # volumes = self.dict_centroids_and_volumes_by_region[region][1]
     
+    # For the initialization of the matrices, we need to determine the cell count for a snap. That is because, each field in the provided list_field_paths may have different mesh. 
+    # Moreover, we need to store the start_index and the end_index of a field from how the fields are assigned in the matrix. This index pair for each field serves as the decryption key for this matrix encryption.
+
     def _determine_tot_cells_per_snap(self):
         self.tot_cells_per_snap = 0
         self.list_field_to_Ncells = []
@@ -59,7 +70,8 @@ class Snapshot_matrix_builder:
             self.tot_cells_per_snap += self.list_field_to_Ncells[index_field]
             index_end = self.tot_cells_per_snap - 1
             self.list_field_to_range_cells.append((index_start, index_end))
-
+            
+    #This method assembles the matrices.
     def _build_snapshot_matrices(self, list_snap_paths):
         snapshot_matrix_2D = np.zeros((self.tot_cells_per_snap, self.Nsnapshots_training))
         snapshot_matrix_3D = []
